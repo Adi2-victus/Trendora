2180c69b69de11bb3df4d7e9bd25a8fed4ae4791
Aditya Darade <adityadarade682005@gmail.com>
2026-09-17 23:06:39 +0530

    solving vercel issue

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5173", "https://trendora-zsay.vercel.app/"],
+  origin: ["http://localhost:5173", "https://trendora-zsay.vercel.app"],
   credentials: true
 }))
 
